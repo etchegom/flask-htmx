fd7eb948abfbb79e3dcfcef196985524e6fefc6d
Etchegoyen Matthieu <etchegom@gmail.com>
2023-08-29 09:16:56 +0200

    assets integration cleanup

--- flask_app/extensions.py
+++ flask_app/extensions.py
@@ -4,9 +4,13 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_assets import Bundle, Environment
 
 db = SQLAlchemy()
 migrate = Migrate()
 debug_toolbar = DebugToolbarExtension()
 htmx = HTMX()
 admin = Admin(name="Backoffice", template_mode="bootstrap3")
+
+assets = Environment()
+assets.register("css", Bundle("src/main.css", output="dist/main.css"))
--- flask_app/main.py
+++ flask_app/main.py
@@ -1,23 +1,14 @@
 from flask import Flask
-from flask_assets import Bundle, Environment
 
 from flask_app.todos.views import blueprint as todos_bp
-from flask_app.extensions import admin, db, debug_toolbar, htmx, migrate
+from flask_app.extensions import admin, db, debug_toolbar, htmx, migrate, assets
 
 
 def create_app(config_object: str = "flask_app.settings") -> Flask:
     app = Flask(__name__)
     app.config.from_object(config_object)
-
     register_extensions(app)
     register_blueprints(app)
-
-    # TODO: move to somewhere else
-    assets = Environment(app)
-    css = Bundle("src/main.css", output="dist/main.css")
-    assets.register("css", css)
-    css.build()
-
     return app
 
 
@@ -27,6 +18,7 @@ def register_extensions(app: Flask) -> None:
     admin.init_app(app)
     debug_toolbar.init_app(app)
     htmx.init_app(app)
+    assets.init_app(app)
 
 
 def register_blueprints(app: Flask) -> None:
