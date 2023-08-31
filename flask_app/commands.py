1144f9f90abf165ddeef4c5edf2222bc6721eea1
Etchegoyen Matthieu <etchegom@gmail.com>
2023-08-31 10:54:56 +0200

    WIP

--- flask_app/commands.py
+++ flask_app/commands.py
@@ -1,8 +1,11 @@
 from click import command
+from flask_app.extensions import db
 from flask_app.todos.factories import TodoFactory
+from flask_app.todos.models import Todo
 
 
 @command(name="populate-db")
 def populate_db():
-    print("Populating the database...")
+    print("Removing all todos and creating 100 new ones.")
+    db.session.query(Todo).delete()
     TodoFactory.create_batch(100)
